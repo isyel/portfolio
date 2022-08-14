2ef801445abf5b31fe2e4b4095a069a67a312f37
isyel <isykaal4saviour@gmail.com>
2022-08-14 16:56:46 +0100

    update data texts - add new experience

--- src/data/data.js
+++ src/data/data.js
@@ -1,80 +1,101 @@
 /* eslint-disable no-multi-str */
 export const dataExperience = {
-	list: {
-		0: {
-			main: "Frontend Engineer",
-		},
-		1: {
-			main: "Software Engineer",
-		},
-		2: {
-			main: "Web Developer",
-		},
-		3: {
-			main: "Java Developer",
-		},
-	},
-	info: {
-		0: {
-			link: "http://innoloft.com/",
-			title: "@Innoloft GmBH 💼",
-			date: "Feb 2021 - present",
-			first:
-				"I serve as a frontend developer where my primary objective was the development of a pipeline tool \
-				(Similar to Trello and Jira Board) that was used internally by the innoloft team, \
+  list: {
+    0: {
+      main: "Frontend Developer",
+    },
+    1: {
+      main: "Frontend Engineer",
+    },
+    2: {
+      main: "Software Engineer",
+    },
+    3: {
+      main: "Web Developer",
+    },
+    4: {
+      main: "Intern Java Developer",
+    },
+  },
+  info: {
+    0: {
+      link: "https://www.360dialog.com/",
+      title: "@360dialog GmBH, Berlin 💼",
+      date: "Oct 2021 - present",
+      first:
+        "Worked as an important member of the frontend team, working the 360 hub for clients and partners. \
+				Created dashboard for visualizing conversation metrics between business and their customers, \
+				providing insights into conversation types (Free and paid) and also billing period.\
+				Created an easy way to fund an account and create a subscription based on users' \
+				expected conversations over a month's period.",
+      second:
+        "Front-end: React, Hooks, Cypress, Storybook, MobX, Styled-components.",
+      third: "Tools and Plugins: Git, Bitbucket, Jira, Slack, Figma.",
+    },
+    1: {
+      link: "http://innoloft.com/",
+      title: "@Innoloft GmBH, Aachen 💼",
+      date: "Feb 2021 - present",
+      first:
+        "I led the development of the two important tools currently used at Innoloft,\
+				A pipeline tool (Similar to Trello and Jira Board) and a forms tool(Similar to Google forms), \
+				that can be shared via a link, questions answered, and responses analysed and viewed. \
+				The tool was also integrated to various parts of the Innoloft platform, an application form. \
+				There tools are currently being used internally by the innoloft team, \
 				and also offered as a premium service (LoftOS) to Innoloft partners.",
-			second:
-				"Front-end: React, Hooks, Redux, Restful Apis, Git & Github, Jira, Google Workspaces.",
-			third: "Tools and Plugins: Draftjs, React-beautiful-DND",
-		},
-		1: {
-			link: "https://web.ecagon.com/",
-			title: "@Ecagon Technologies 🖥",
-			date: "Feb 2019 - Dec 2020",
-			first:
-				"I served as a Technical Team Lead for the Product development in over 5 projects across different industries(Ecommerce, Media and Real Estate). \
+      second:
+        "Front-end: React, Hooks, Redux, Restful Apis, Draftjs, React-beautiful-DND, localization ",
+      third: "Tools and Plugins: Git & Github, Jira, Miro, Google Workspaces.",
+    },
+    2: {
+      link: "https://web.ecagon.com/",
+      title: "@Ecagon Technologies 🖥",
+      date: "Feb 2019 - Dec 2020",
+      first:
+        "I served as a Technical Team Lead for the Product development in over 5 projects across different industries(Ecommerce, Media and Real Estate). \
 				My primary objective was ensuring that feature implementations went smoothly from business requirement to technical implementation, \
 				without deviating from the expectations expressed (or implied) by the product owners.",
-			second:
-				"Front-end: React, Angular, Ionic, Redux, Context Api, Bootstrap, Material-UI, Restful Api, Git & Github, Bitbucket",
-			third: "Back-end: Php/Laravel, Nodejs",
-			fourth: "Testing: Jest, React Testing Library",
-		},
-		2: {
-			link: "https://caltech-ltd.com/",
-			title: "@Caltech Integrated Services 🏗",
-			date: "Jan 2018 - Aug 2019",
-			first:
-				"I served as a Web Engineer for the software development team. \
+      second:
+        "Front-end: React, Angular, Ionic, Redux, Context Api, Bootstrap, Material-UI, Restful Api, Git & Github, Bitbucket",
+      third: "Back-end: Php/Laravel, Nodejs",
+      fourth: "Testing: Jest, React Testing Library",
+    },
+    3: {
+      link: "https://caltech-ltd.com/",
+      title: "@Caltech Integrated Services 🏗",
+      date: "Jan 2018 - Aug 2019",
+      first:
+        "I served as a Web Engineer for the software development team. \
 				I worked on developing front facing web applications for Caltech clients and also helped to build applications to manage day to day activities for companies across the Oil & Gas and Bio Medicals Industry, which Caltech Integrated serves.",
-			second: "Technologies: Php/Laravel, Wordpress, Angular, React, Ionic",
-			third: "Other Technologies Used: Odoo, Python/Flask",
-		},
-		3: {
-			link: "https://cwg-plc.com/",
-			title: "@CWG, Plc  🤖",
-			date: "Feb 2017 - Dec 2017",
-			first:
-				"Interned with Computer Warehouse Group[CWG] Lagos, which helped me develop my knowledge across various fields one of which was a Banking Application(Finacle by Infosys).\
+      second: "Technologies: Php/Laravel, Wordpress, Angular, React, Ionic",
+      third: "Other Technologies Used: Odoo, Python/Flask",
+    },
+    4: {
+      link: "https://cwg-plc.com/",
+      title: "@CWG, Plc  🤖",
+      date: "Feb 2017 - Dec 2017",
+      first:
+        "Interned with Computer Warehouse Group[CWG] Lagos, which helped me develop my knowledge across various fields one of which was a Banking Application(Finacle by Infosys).\
 			In my latter days i engaged with the software development team working with Java and Android Studio",
-			second:
-				"Key Accomplishments: Supported the team that developed “bills & pay” mobile application using Java, \
+      second:
+        "Key Accomplishments: Supported the team that developed “bills & pay” mobile application using Java, \
 			Worked with Version Control (Git) to manage projects across board (Bitbucket)",
-		},
-	},
+      third: "Tools Used: Java, Github, Android Studio",
+    },
+  },
 };
 
 export const dataEducation = {
-	0: {
-		main: "Karunya University, IN",
-		expand: "Computer Science Engineering",
-		date: "( 2012 - 2016 )",
-		country: "India",
-	},
-	1: {
-		main: "IMT Enugu, NG",
-		expand: "Mechanical Engineer Diploma",
-		date: "( 2009 - 2011 )",
-		country: "Nigeria",
-	},
+  0: {
+    main: "Karunya University, IN",
+    expand: "Computer Science Engineering",
+    date: "( 2012 - 2016 )",
+    country: "India",
+  },
+  1: {
+    main: "IMT Enugu, NG",
+    expand: "Mechanical Engineer Diploma",
+    date: "( 2009 - 2011 )",
+    country: "Nigeria",
+  },
 };
